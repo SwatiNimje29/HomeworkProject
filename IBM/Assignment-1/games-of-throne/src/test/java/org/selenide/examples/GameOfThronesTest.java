77699bab7c770009af04947564a57cd0a88f04a5
SwatiNimje29 <82224678+SwatiNimje29@users.noreply.github.com>
2021-04-13 21:01:12 +0530

    Add files via upload
    Added Assertion

--- IBM/Assignment-1/games-of-throne/src/test/java/org/selenide/examples/GameOfThronesTest.java
+++ IBM/Assignment-1/games-of-throne/src/test/java/org/selenide/examples/GameOfThronesTest.java
@@ -1,9 +1,12 @@
 package org.selenide.examples;
 import com.codeborne.selenide.*;
 import org.apache.hc.core5.util.Timeout;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.CollectionCondition.empty;
 import static com.codeborne.selenide.CollectionCondition.size;
@@ -11,17 +14,26 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GameOfThronesTest {
+    By suggestionSearch = By.id("suggestion-search");
+    By findList = By.className("findList");
+    By slate = By.className("slate");
+
     @Test
-    public void user (){
+    public void user () throws InterruptedException {
+
+        String pageTitle = "Game of Thrones (Video Game 2012) - IMDb";
+        String suggestionValue = "games of thrones";
+
         open("https://imdb.com");
         Selenide.screenshot("imdb");
-        $(By.id("suggestion-search")).setValue("games of thrones") .pressEnter();
+        $(suggestionSearch).setValue(suggestionValue) .pressEnter();
         Selenide.screenshot("searched-games-of-throne");
-        System.out.println("Games of thrones video page url ="+ $(By.className("findList")).find("tbody:first-of-type").findAll("td").find(Condition.matchText("Video Game")).find("a").attr("href"));
-        $(By.className("findList")).find("tbody:first-of-type").findAll("td").find(Condition.matchText("Video Game")).find("a").click();
+        System.out.println("Games of thrones video page url ="+ $(findList).find("tbody:first-of-type").findAll("td").find(Condition.matchText("Video Game")).find("a").attr("href"));
+        $(findList).find("tbody:first-of-type").findAll("td").find(Condition.matchText("Video Game")).find("a").click();
         Selenide.screenshot("searched-games-of-throne-url");
-        System.out.println("Video Element url = " +$(By.className("slate")).find("a").attr("href"));
-        $(By.className("slate")).find("a").click();
+        System.out.println("Video Element url = " +$(slate).find("a").attr("href"));
+       // $(By.className("video-player__video")).shouldBe(Condition.visible, Duration.ofMinutes(3));
+        Assert.assertEquals(pageTitle,title());
         closeWindow();
     }
 }
